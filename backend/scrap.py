736209756fd4ff73f91a71d4fa6194cd5aa00487
Shivam Raj <shivamraj.kth@gmail.com>
2025-08-22 21:49:01 +0530

    Shivam-Assignment

--- backend/scrap.py
+++ backend/scrap.py
@@ -1,39 +1,3 @@
-# import os
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
-# import pandas as pd
-
-# url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
-# r = requests.get(url)
-
-# soup = BeautifulSoup(r.text, "lxml")
-
-# # Extract names
-# names = soup.find_all("a", class_="title")
-# # Extract prices
-# prices = soup.find_all("h4", class_="price float-end card-title pull-right")
-
-
-# # Open CSV file to write
-# with open("products.csv", "w", newline="", encoding="utf-8") as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["Name", "Price"])  # header row
-
-#     # Write data row by row
-#     for name, price in zip(names, prices):
-#         writer.writerow([name.text.strip(), price.text.strip()])
-   
-# print("✅ Data saved to products.csv")
-
-# df = pd.read_csv("products.csv")
-# print(df)
-
-# df.to_csv("D:/scraped data/scrapded_data.csv")
-
-
-
-
 
 import requests
 from bs4 import BeautifulSoup
--- backend/webscraping.py
+++
@@ -1,8 +0,0 @@
-# import pandas as pd
-# import requests 
-# from bs4 import BeautifulSoup
-
-# url ="https://www.flipkart.com"
-
-# r = requests.get(url)
-# print(r)
